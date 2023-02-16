b5dde27a457d776254b83cc53969027416acbaca
양현승 <happyman1104@naver.com>
2023-02-17 00:06:28 +0900

    dometic init

---
+++ domestic/src/main/java/com/sixtythree/stock63/domestic/DomesticApplication.java
@@ -0,0 +1,13 @@
+package com.sixtythree.stock63.domestic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomesticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomesticApplication.class, args);
+	}
+
+}
---
+++ domestic/src/test/java/com/sixtythree/stock63/domestic/DomesticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sixtythree.stock63.domestic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomesticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
