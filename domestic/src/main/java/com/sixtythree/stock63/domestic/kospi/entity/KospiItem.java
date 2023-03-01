40c5e6779a3b5c821897dc14683af0ca8c9b2c0c
양현승 <happyman1104@naver.com>
2023-03-01 22:33:59 +0900

    [domestic] swagger 설명추가

--- domestic/src/main/java/com/sixtythree/stock63/domestic/kospi/entity/KospiItem.java
+++ domestic/src/main/java/com/sixtythree/stock63/domestic/kospi/entity/KospiItem.java
@@ -1,5 +1,6 @@
 package com.sixtythree.stock63.domestic.kospi.entity;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
@@ -7,15 +8,24 @@ import lombok.Data;
 
 @Entity
 @Data
+@Schema(description = "코스피 종목 정보")
 public class KospiItem {
+    @Schema(description = "단축코드")
     private String mkscShrnIscd;
+    @Schema(description = "표준코드")
     @Id
     private String stndIscd;
+    @Schema(description = "한글명")
     private String htsKorIsnm;
+    @Schema(description = "그룹코드")
     private String scrtGrpClsCode;
+    @Schema(description = "한글명")
     private String avlsScalClsCode;
+    @Schema(description = "지수업종대분류")
     private String bstpLargDivCode;
+    @Schema(description = "지수업종중분류")
     private String bstpMedmDivCode;
+    @Schema(description = "지수업종소분류")
     private String bstpSmalDivCode;
     private String mninClsCodeYn;
     private String lowCurrentYn;
@@ -47,8 +57,11 @@ public class KospiItem {
     private String krxInsuYn;
     private String krxTrnpYn;
     private String sriNmixYn;
+    @Schema(description = "기준가")
     private String stckSdpr;
+    @Schema(description = "매매수량단위")
     private String frmlMrktDealQtyUnit;
+    @Schema(description = "시간외수량단위")
     private String ovtmMrktDealQtyUnit;
     private String trhtYn;
     private String sltrYn;
@@ -63,10 +76,15 @@ public class KospiItem {
     private String margRate;
     private String crdtAble;
     private String crdtDays;
+    @Schema(description = "전일거래량")
     private String prdyVol;
+    @Schema(description = "액면가")
     private String stckFcam;
+    @Schema(description = "상장일자")
     private String stckLstnDate;
+    @Schema(description = "상장주수")
     private String lstnStcn;
+    @Schema(description = "자본금")
     private String cpfn;
     private String stacMonth;
     private String poPrc;
@@ -75,12 +93,17 @@ public class KospiItem {
     private String stangeRunupYn;
     private String krx300IssuYn;
     private String kospiIssuYn;
+    @Schema(description = "매출액")
     private String saleAccount;
+    @Schema(description = "영업이익")
     private String bsopPrfi;
+    @Schema(description = "경상이익")
     private String opPrfi;
+    @Schema(description = "단기순이익")
     private String thtrNtin;
     private String roe;
     private String baseDate;
+    @Schema(description = "시가총액")
     private String prdyAvlsScal;
     private String grpCode;
     private String coCrdtLimtOverYn;
--- domestic/src/main/java/com/sixtythree/stock63/domestic/kospi/web/KospiController.java
+++ domestic/src/main/java/com/sixtythree/stock63/domestic/kospi/web/KospiController.java
@@ -1,6 +1,7 @@
 package com.sixtythree.stock63.domestic.kospi.web;
 
 import com.sixtythree.stock63.domestic.kospi.entity.KospiItem;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -21,7 +22,9 @@ public class KospiController {
     @ResponseBody
     @GetMapping("list")
     public ResponseEntity<List<KospiItem>> rankList(
+            @Parameter(description = "연속 상승/하락 일수")
             @RequestParam(name ="period",required = false) int period,
+            @Parameter(description = "상승: 1 / 하락: -1")
             @RequestParam(name="gradient",required = false) int gradient
             ) {
         return kospiService.stockList(period, gradient);
