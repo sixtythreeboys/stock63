f1e2d45fc91102f8b5dc1733a297de029b2f4444
Lee Han Seong <jkhk7788@gmail.com>
2023-03-04 13:39:10 +0900

    oversea remove

--- oversea/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { OverseaModule } from './oversea/oversea.module';
-
-@Module({
-  imports: [OverseaModule],
-  controllers: [],
-  providers: [],
-})
-export class AppModule {}
