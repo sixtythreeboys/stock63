d3e3e3101292c94c755fd0c47d3a1ae4129f92ca
양현승 <happyman1104@naver.com>
2023-07-02 17:07:13 +0900

    fix

--- domestic/scripts/start.sh
+++ domestic/scripts/start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PROJECT_ROOT="/home/ubuntu/stock63/domestic"
+PROJECT_ROOT="/home/ubuntu/app/domestic"
 JAR_FILE="$PROJECT_ROOT/build/libs/domestic-0.0.1-SNAPSHOT.jar"
 
 APP_LOG="$PROJECT_ROOT/application.log"
